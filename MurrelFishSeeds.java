8e27d50d7f75ae32b9544641b39eea2d566d379b
josephyesebu37-rgb <josephyesebu37@gmail.com>
2025-08-10 00:49:51 +0530

    Add files via upload

---
+++ MurrelFishSeeds.java
@@ -0,0 +1,7 @@
+public class MurrelFishSeeds {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Murrel Fish Seeds Supplier");
+        System.out.println("We provide healthy and certified Murrel fish seeds.");
+        System.out.println("Contact us at +91 99999 99999 for orders.");
+    }
+}
